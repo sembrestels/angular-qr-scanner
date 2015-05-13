1bb41065862cf52f10930bc06b254b5b12064d74
Sem <sembrestels@riseup.net>
2015-05-13 20:13:23 +0200

    Added demo.

---
+++ bower_components/angular-qr-scanner/Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            dist: {
+                files: {
+                    'src/jsqrcode-combined.min.js': [
+                        "src/jsqrcode/src/grid.js",
+                        "src/jsqrcode/src/version.js",
+                        "src/jsqrcode/src/detector.js",
+                        "src/jsqrcode/src/formatinf.js",
+                        "src/jsqrcode/src/errorlevel.js",
+                        "src/jsqrcode/src/bitmat.js",
+                        "src/jsqrcode/src/datablock.js",
+                        "src/jsqrcode/src/bmparser.js",
+                        "src/jsqrcode/src/datamask.js",
+                        "src/jsqrcode/src/rsdecoder.js",
+                        "src/jsqrcode/src/gf256poly.js",
+                        "src/jsqrcode/src/gf256.js",
+                        "src/jsqrcode/src/decoder.js",
+                        "src/jsqrcode/src/qrcode.js",
+                        "src/jsqrcode/src/findpat.js",
+                        "src/jsqrcode/src/alignpat.js",
+                        "src/jsqrcode/src/databr.js",
+                    ]
+                },
+                options: {
+                    beautify: false,
+                    mangle: false,
+                    sourceMap: false
+                }
+            }
+        },
+        watch: {
+            js: {
+                files: ['src/*.js']
+            }
+        }
+    });
+
+    // Load the plugin that provides the tasks.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', [
+        'uglify'
+    ]);
+};
---
+++ bower_components/angular-qr-scanner/README.md
@@ -0,0 +1,45 @@
+angular-qr-scanner
+==================
+
+Angular directive for a QR Scanner. It is the angular version of [html5-qrcode](https://github.com/dwa012/html5-qrcode) and uses [jsqrcode](https://github.com/LazarSoft/jsqrcode).
+
+### Usage
+
+```html
+<qr-scanner ng-success="onSuccess(data)" width="400" height="300"></qr>
+```
+
+### Demo
+```html
+<html ng-app="App">
+<body ng-controller="qrCrtl">
+<qr-scanner width="400" height="300" ng-success="onSuccess(data)" ng-error="onError(error)" />
+
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular.js"></script>
+<script src="qr-scanner.js"></script>
+<script src="src/jsqrcode-combined.min.js"></script>
+<script>
+
+var App = angular.module('App', ['qrScanner']);
+
+App.controller('qrCrtl', ['$scope', function($scope) {
+    $scope.onSuccess = function(data) {
+        console.log(data);
+    };
+    $scope.onError = function(error) {
+        console.log(error);
+    };
+    $scope.onVideoError = function(error) {
+        console.log(error);
+    };
+}]);
+
+</script>
+</body>
+</html>
+```
+
+### License
+The MIT License
+
+Copyright (c) 2013-2015 Sembrestels
