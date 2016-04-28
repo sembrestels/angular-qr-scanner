b34bc1102274051a2ce3909789924ba03631e837
Thomas Manson <dev.mansonthomas@gmail.com>
2016-04-28 08:02:14 +0200

    $window.localMediaStream.stop(); not available on Chrome
    This don't work anymore in Google Chrome
    https://developers.google.com/web/updates/2015/07/mediastream-deprecations?hl=en#stop-ended-and-active
    $window.localMediaStream.stop();
    and needs to be replaced by this : (which works one Firefox & Chrome)
    $window.localMediaStream.getTracks()[0].stop();

--- qr-scanner.js
+++ qr-scanner.js
@@ -72,7 +72,7 @@ angular.module('qrScanner', ["ng"]).directive('qrScanner', ['$interval', '$windo
 
       element.bind('$destroy', function() {
         if ($window.localMediaStream) {
-          $window.localMediaStream.stop();
+          $window.localMediaStream.getTracks()[0].stop();
         }
         if (stopScan) {
           $interval.cancel(stopScan);
